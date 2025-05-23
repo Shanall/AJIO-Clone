f3f16222b0d393570566b83d84387b564e0cfbe2
Mrinal Rai <153422116+Shanall@users.noreply.github.com>
2025-05-23 22:56:11 +0530

    Add files via upload

---
+++ hello.js
@@ -0,0 +1,23 @@
+console.log("Hello world");
+alert("just a basic thing");
+var name="mri";
+console.log(name);
+name=5;
+console.log(name);
+let firstName="abc"
+console.log(firstName);
+let lastName="abc"
+console.log(lastName);
+
+let firstName="abc"
+console.log(firstName + " " + lastName);
+let sum=0;
+for(var i=0;i<=50;i++)
+{
+if(i%2==1)
+{
+    sum= sum+i;
+}
+}
+console.log(sum);
+console.log("i =" + i);1
